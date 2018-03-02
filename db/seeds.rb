de150ff098cd4fee30fdef140b35455a8f29fdb3
Windu Risky Adiatma <windu.risky@gmail.com>
2018-03-02 16:29:23 +0700

    add new seed and database cleaner on seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,6 @@
 require './config/environment'
 
+DatabaseCleaner.clean_with(:truncation)
 load 'db/seeds/books.rb'
 load 'db/seeds/members.rb'
 load 'db/seeds/rents.rb'
--- db/seeds/books.rb
+++ db/seeds/books.rb
@@ -7,3 +7,13 @@ Book.create(
   category: "Programming",
   price: 150_000
 )
+
+Book.create(
+  title: "Learn Ruby on Rails 4",
+  description: "Mempelajari ruby on rails 4",
+  isbn: "0000000000000000",
+  release_year: 2018,
+  publisher: "Gramedia",
+  category: "Programming",
+  price: 150_000
+)
